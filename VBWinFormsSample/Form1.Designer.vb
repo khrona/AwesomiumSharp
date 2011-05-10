f80a380e7af9904c986a595c1742436e8d3b0199
Robert  Hoyt <robert@khrona.com>
2011-05-10 12:32:44 -0500

    Added VBWinFormsSample Project

---
+++ VBWinFormsSample/Form1.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.webViewBitmap = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.webViewBitmap, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'webViewBitmap
+        '
+        Me.webViewBitmap.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.webViewBitmap.Location = New System.Drawing.Point(0, 0)
+        Me.webViewBitmap.Name = "webViewBitmap"
+        Me.webViewBitmap.Size = New System.Drawing.Size(532, 533)
+        Me.webViewBitmap.TabIndex = 0
+        Me.webViewBitmap.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 30
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(532, 533)
+        Me.Controls.Add(Me.webViewBitmap)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.webViewBitmap, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents webViewBitmap As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ VBWinFormsSample/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports AwesomiumSharp
+Imports System.Runtime.InteropServices
+Imports System.Drawing.Imaging
+Imports Microsoft.VisualBasic
+
+Public Class Form1
+    Dim webView As WebView
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim config As WebCore.Config = New WebCore.Config()
+        config.enablePlugins = True
+
+        WebCore.Initialize(config)
+        webView = WebCore.CreateWebview(webViewBitmap.Width, webViewBitmap.Height)
+        webView.LoadURL("http://www.google.com")
+        webView.Focus()
+    End Sub
+
+    Dim bmp As Bitmap
+    Private Sub render()
+        Dim rBuffer As RenderBuffer = webView.Render()
+
+        Dim data(webViewBitmap.Width * webViewBitmap.Height) As Integer
+        Marshal.Copy(rBuffer.GetBuffer(), data, 0, webViewBitmap.Width * webViewBitmap.Height)
+
+        bmp = New Bitmap(webViewBitmap.Width, webViewBitmap.Height, PixelFormat.Format32bppArgb)
+
+        Dim bits As BitmapData = bmp.LockBits(New Rectangle(0, 0, webViewBitmap.Width, webViewBitmap.Height), ImageLockMode.ReadWrite, bmp.PixelFormat)
+
+        Dim ptr As IntPtr = bits.Scan0.ToInt32()
+        rBuffer.CopyTo(ptr, bits.Stride, 4, False)
+
+        bmp.UnlockBits(bits)
+
+        webViewBitmap.Image = bmp
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        WebCore.Update()
+        If webView.IsDirty Then
+            render()
+        End If
+    End Sub
+
+    Public Sub webViewBitmap_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles webViewBitmap.MouseDown
+        webView.InjectMouseDown(MouseButton.Left)
+    End Sub
+
+    Public Sub webViewBitmap_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles webViewBitmap.MouseMove
+        webView.InjectMouseMove(e.X, e.Y)
+    End Sub
+
+    Public Sub webViewBitmap_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles webViewBitmap.MouseUp
+        webView.InjectMouseUp(MouseButton.Left)
+    End Sub
+
+    Public Sub webViewBitmap_MouseWheel(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseWheel
+        webView.InjectMouseWheel(e.Delta)
+    End Sub
+
+    Public Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        Dim keyEvent As WebKeyboardEvent = New WebKeyboardEvent
+        keyEvent.type = WebKeyType.KeyDown
+        keyEvent.virtualKeyCode = e.KeyCode
+        webView.InjectKeyboardEvent(keyEvent)
+    End Sub
+
+    Private Sub Form1_KeyPress(ByVal sender As Object, ByVal e As KeyPressEventArgs) Handles MyBase.KeyPress
+        Dim keyEvent As WebKeyboardEvent = New WebKeyboardEvent
+        keyEvent.type = WebKeyType.Char
+        keyEvent.text = New UShort() {Asc(e.KeyChar), 0, 0, 0}
+        webView.InjectKeyboardEvent(keyEvent)
+    End Sub
+
+    Public Sub Form1_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
+        Dim keyEvent As WebKeyboardEvent = New WebKeyboardEvent
+        keyEvent.type = WebKeyType.KeyUp
+        keyEvent.virtualKeyCode = e.KeyCode
+        webView.InjectKeyboardEvent(keyEvent)
+    End Sub
+
+    Private Sub Form1_SizeChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.ResizeEnd
+        webView.Resize(webViewBitmap.Width, webViewBitmap.Height)
+    End Sub
+End Class
